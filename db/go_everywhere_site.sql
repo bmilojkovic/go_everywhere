cc54740432c59cff840db7d38f57ced12fc5c4c9
ludipingvin <bandza88@gmail.com>
2017-02-08 13:03:23 +0100

    Removing chattable from db

--- db/go_everywhere_site.sql
+++ db/go_everywhere_site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 04, 2017 at 12:42 PM
+-- Generation Time: Feb 08, 2017 at 11:41 AM
 -- Server version: 10.1.20-MariaDB
 -- PHP Version: 7.0.15
 
@@ -23,24 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chattable`
---
-
-CREATE TABLE `chattable` (
-  `id` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `chattable`
---
-
-INSERT INTO `chattable` (`id`) VALUES
-(1),
-(2);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `chat_line`
 --
 
@@ -50,24 +32,6 @@ CREATE TABLE `chat_line` (
   `line_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `text` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `chat_line`
---
-
-INSERT INTO `chat_line` (`id`, `chat_room_id`, `line_datetime`, `text`) VALUES
-(1, 1, '2017-02-04 09:47:59', 'qwe'),
-(2, 1, '2017-02-04 10:00:05', 'qwe'),
-(3, 1, '2017-02-04 10:01:12', 'qwe'),
-(4, 1, '2017-02-04 10:01:34', 'asd'),
-(5, 1, '2017-02-04 10:01:59', 'asd'),
-(6, 1, '2017-02-04 10:03:30', 'zzz'),
-(7, 1, '2017-02-04 10:03:46', 'zzz'),
-(8, 1, '2017-02-04 10:04:40', 'zzz'),
-(9, 1, '2017-02-04 10:05:01', 'zzz'),
-(10, 1, '2017-02-04 10:05:05', 'zzzeee'),
-(11, 1, '2017-02-04 10:05:16', 'zzzeeeaa'),
-(12, 1, '2017-02-04 10:20:49', 'QWE');
 
 -- --------------------------------------------------------
 
@@ -96,10 +60,10 @@ INSERT INTO `chat_room` (`id`, `name`) VALUES
 
 CREATE TABLE `game` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `chattable_id` bigint(20) UNSIGNED NOT NULL,
+  `chat_room_id` bigint(20) UNSIGNED NOT NULL,
   `room_id` bigint(20) UNSIGNED NOT NULL,
-  `player_white_id` bigint(20) UNSIGNED NOT NULL,
-  `player_black_id` bigint(20) UNSIGNED NOT NULL,
+  `player_white_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `player_black_id` bigint(20) UNSIGNED DEFAULT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `type` char(1) COLLATE utf8_unicode_ci NOT NULL,
   `status` char(1) COLLATE utf8_unicode_ci NOT NULL,
@@ -126,14 +90,6 @@ CREATE TABLE `player` (
   `domain` char(3) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `player`
---
-
-INSERT INTO `player` (`id`, `user_id`, `name`, `go_rank`, `domain`) VALUES
-(1, 1, 'pro', NULL, 'GOE'),
-(2, 2, 'Name', NULL, 'GOE');
-
 -- --------------------------------------------------------
 
 --
@@ -142,7 +98,7 @@ INSERT INTO `player` (`id`, `user_id`, `name`, `go_rank`, `domain`) VALUES
 
 CREATE TABLE `room` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `chattable_id` bigint(20) UNSIGNED NOT NULL,
+  `chat_room_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -160,22 +116,8 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `facebook_id`, `display_name`, `email`) VALUES
-(1, 1, 'pro', 'ema'),
-(2, 123, 'Name', 'email@site.com');
-
---
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `chattable`
---
-ALTER TABLE `chattable`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `chat_line`
@@ -195,10 +137,10 @@ ALTER TABLE `chat_room`
 --
 ALTER TABLE `game`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `chattable_id` (`chattable_id`),
   ADD KEY `room_id` (`room_id`),
   ADD KEY `game_ibfk_3` (`player_white_id`),
-  ADD KEY `game_ibfk_4` (`player_black_id`);
+  ADD KEY `game_ibfk_4` (`player_black_id`),
+  ADD KEY `chat_room_id` (`chat_room_id`);
 
 --
 -- Indexes for table `player`
@@ -212,7 +154,7 @@ ALTER TABLE `player`
 --
 ALTER TABLE `room`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `chattable_id` (`chattable_id`);
+  ADD KEY `chat_room_id` (`chat_room_id`);
 
 --
 -- Indexes for table `user`
@@ -225,15 +167,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `chattable`
---
-ALTER TABLE `chattable`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- AUTO_INCREMENT for table `chat_line`
 --
 ALTER TABLE `chat_line`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `game`
 --
@@ -243,7 +180,7 @@ ALTER TABLE `game`
 -- AUTO_INCREMENT for table `player`
 --
 ALTER TABLE `player`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `room`
 --
@@ -253,7 +190,7 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -265,19 +202,13 @@ ALTER TABLE `chat_line`
   ADD CONSTRAINT `chat_line_ibfk_1` FOREIGN KEY (`chat_room_id`) REFERENCES `chat_room` (`id`);
 
 --
--- Constraints for table `chat_room`
---
-ALTER TABLE `chat_room`
-  ADD CONSTRAINT `chat_room_ibfk_1` FOREIGN KEY (`id`) REFERENCES `chattable` (`id`);
-
---
 -- Constraints for table `game`
 --
 ALTER TABLE `game`
-  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`chattable_id`) REFERENCES `chattable` (`id`),
   ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`),
   ADD CONSTRAINT `game_ibfk_3` FOREIGN KEY (`player_white_id`) REFERENCES `player` (`id`),
-  ADD CONSTRAINT `game_ibfk_4` FOREIGN KEY (`player_black_id`) REFERENCES `player` (`id`);
+  ADD CONSTRAINT `game_ibfk_4` FOREIGN KEY (`player_black_id`) REFERENCES `player` (`id`),
+  ADD CONSTRAINT `game_ibfk_5` FOREIGN KEY (`chat_room_id`) REFERENCES `chat_room` (`id`);
 
 --
 -- Constraints for table `player`
@@ -289,7 +220,7 @@ ALTER TABLE `player`
 -- Constraints for table `room`
 --
 ALTER TABLE `room`
-  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`chattable_id`) REFERENCES `chattable` (`id`);
+  ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`chat_room_id`) REFERENCES `chat_room` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
