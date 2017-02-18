9aaf9d84306efcafe9dd4b2dcc9ad91fc11e39dc
Nikola Rajkov <nrajkov@smartcodehouse.com>
2017-02-18 12:57:26 +0100

    Player *= 1; Djimka nam ulepsava kod. Salu na stranu, dodato stanje table i resen preview poteza.

--- ge_fb_client/board.js
+++ ge_fb_client/board.js
@@ -1,64 +1,92 @@
 var canvas;
 var ctx;
-var img;
+var img = [];
+var boardState = []; // empty: 0, black: 1, white: -1
 var mousePosition = [];
 var fieldCoords = [];
+var nearestFieldCoords;
+var currentPlayer = 1;
 
 function Init()
 {
 	canvas = document.getElementById("board");
 	ctx = canvas.getContext("2d");
+	img[0] = document.getElementById("white-stone");
+	img[1] = document.getElementById("board-image");
+	img[2] = document.getElementById("black-stone");
+	SetInitialBoardState();
+	
 	DrawBoard();
-	img = document.getElementById("black-stone");
 	
 	canvas.addEventListener('mousedown', function(evt)
 	{
-		mousePosition = GetMousePosition(canvas, evt);
+		if (IsMoveLegal())
+		{
+			PlaceStone();
+		}
     }, false);
 	canvas.addEventListener('mousemove', function(evt)
 	{
 		mousePosition = GetMousePosition(canvas, evt);
     }, false);
+	
+}
+
+function SetInitialBoardState()
+{
+	for (var i = 0; i < 19; i++)
+	{
+		boardState[i] = [];
+		for (var j = 0; j < 19; j++)
+		{
+			boardState[i][j] = 0;
+		}
+		fieldCoords[i] = i*28 + 13;
+	}
 }
 
 function DrawBoard()
 {
+	ctx.drawImage(img[1], 0, 0);
+	
 	for (var i = 0; i < 19; i++)
 	{
-		fieldCoords[i] = i*28 + 13;
-		ctx.moveTo(i*28 + 13, 13);
-		ctx.lineTo(i*28 + 13, 518);
-		ctx.moveTo(13, i*28 + 13);
-		ctx.lineTo(518, i*28 + 13);
-	}
-	ctx.stroke();
-
-	for (var i = 0; i < 3; i++)
-	{
-		for (var j = 0; j < 3; j++)
+		for (var j = 0; j < 19; j++)
 		{
-			ctx.moveTo(i*6*28 + 100, j*6*28 + 97);
-			ctx.arc(i*6*28 + 97, j*6*28 + 97, 3, 0, 2*Math.PI);
+			if (boardState[i][j] != 0)
+				ctx.drawImage(img[boardState[i][j] + 1], fieldCoords[i] - 13, fieldCoords[j] - 13, 26, 26);
 		}
 	}
-	ctx.fill();
 }
 
 function ShowMovePreview()
 {
-	var nearestFieldCoords = GetNearestFieldCoords();
-	ctx.drawImage(img, nearestFieldCoords[0] - 13, nearestFieldCoords[1] - 13, 26, 26);
+	var newNearestFieldCoords = GetNearestFieldCoords();
+	if (newNearestFieldCoords != nearestFieldCoords)
+	{
+		nearestFieldCoords = newNearestFieldCoords;
+		DrawBoard();
+		if (IsMoveLegal())
+		{
+			ctx.globalAlpha = 0.7;
+			ctx.drawImage(img[currentPlayer+1], fieldCoords[nearestFieldCoords[0]] - 13, fieldCoords[nearestFieldCoords[1]] - 13, 26, 26);
+			ctx.globalAlpha = 1;
+		}
+	}
+}
+
+function IsMoveLegal()
+{
+	if (boardState[nearestFieldCoords[0]][nearestFieldCoords[1]] != 0)
+		return false;
+	return true;
 }
 
 function PlaceStone()
 {
-	console.log(img);
-	var nearestFieldCoords = GetNearestFieldCoords();
-	ctx.drawImage(img, nearestFieldCoords[0] - 13, nearestFieldCoords[1] - 13, 26, 26);
-	if (img.id != "black-stone")
-		img = document.getElementById("black-stone");
-	else
-		img = document.getElementById("white-stone");
+	boardState[nearestFieldCoords[0]][nearestFieldCoords[1]] = currentPlayer;
+	ctx.drawImage(img[currentPlayer+1], fieldCoords[nearestFieldCoords[0]] - 13, fieldCoords[nearestFieldCoords[1]] - 13, 26, 26);
+	currentPlayer *= -1;
 }
 
 function GetMousePosition(canvas, evt)
@@ -76,9 +104,9 @@ function GetNearestFieldCoords()
 	for(var i = 0; i < 19; i++)
 	{
 		if (fieldCoords[i] >= mousePosition[0] - 14 && fieldCoords[i] <= mousePosition[0] + 14)
-			pos[0] = fieldCoords[i];
+			pos[0] = i;
 		if (fieldCoords[i] >= mousePosition[1] - 14 && fieldCoords[i] <= mousePosition[1] + 14)
-			pos[1] = fieldCoords[i];
+			pos[1] = i;
 	}
 	return pos;
 }
